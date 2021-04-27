4cce43a17f0ba200e7dfe487fa5c4f9ada904100
ndaemy <yuyaebean@gmail.com>
2021-04-27 21:49:59 +0900

    Create _curry, _curryr

--- js/_.js
+++ js/_.js
@@ -22,3 +22,23 @@ function _map(array, mapper) {
   });
   return newArray;
 }
+
+function _curry(fn) {
+  return function (a, b) {
+    return arguments.length === 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(a, b);
+        };
+  };
+}
+
+function _curryr(fn) {
+  return function (a, b) {
+    return arguments.length === 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(b, a);
+        };
+  };
+}
