10bbeea6bb30a18c191d366d76cfe654b99ccbf3
ndaemy <yuyaebean@gmail.com>
2021-04-29 01:03:48 +0900

    Create _rest, _reduce

--- js/_.js
+++ js/_.js
@@ -46,3 +46,19 @@ function _curryr(fn) {
 const _get = _curryr(function (obj, key) {
   return obj ? obj[key] : undefined;
 });
+
+const slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+
+function _reduce(list, iter, memo) {
+  if (arguments.length === 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, function (v) {
+    memo = iter(memo, v);
+  });
+  return memo;
+}
